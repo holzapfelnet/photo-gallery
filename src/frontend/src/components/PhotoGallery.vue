e82f8db677b2dcd9f55c03c94c2b324ad7efc6eb
Sebastian <sebastian@holzapfelnet.com>
2024-09-10 19:53:05 +0200

    Refactoring

--- src/frontend/src/components/PhotoGallery.vue
+++ src/frontend/src/components/PhotoGallery.vue
@@ -18,90 +18,15 @@
 
 <script>
 export default {
+
+
   name: "PhotoGallery",
   data() {
+
+  
     return {
       selectedPhoto: null,
-      photos: [
-        {
-          src: require("@/assets/images/image1-preview.jpg"),
-          fullSrc: require("@/assets/images/image1.jpg"),
-          alt: "Photo 1",
-          width: 600,
-          height: 300,
-        },
-        {
-          src: require("@/assets/images/image2-preview.jpg"),
-          fullSrc: require("@/assets/images/image2.jpg"),
-          alt: "Photo 2",
-          width: 600,
-          height: 300,
-        },
-        {
-          src: require("@/assets/images/image3-preview.jpg"),
-          fullSrc: require("@/assets/images/image3.jpg"),
-          alt: "Photo 3",
-          width: 200,
-          height: 300,
-        },
-        {
-          src: require("@/assets/images/image4-preview.jpg"),
-          fullSrc: require("@/assets/images/image4.jpg"),
-          alt: "Photo 3",
-          width: 400,
-          height: 300,
-        },
-        {
-          src: require("@/assets/images/image5-preview.jpg"),
-          fullSrc: require("@/assets/images/image5.jpg"),
-          alt: "Photo 3",
-          width: 400,
-          height: 300,
-        },
-        {
-          src: require("@/assets/images/image6-preview.jpg"),
-          fullSrc: require("@/assets/images/image6.jpg"),
-          alt: "Photo 3",
-          width: 400,
-          height: 300,
-        },
-        {
-          src: require("@/assets/images/image11-preview.jpg"),
-          fullSrc: require("@/assets/images/image11.jpg"),
-          alt: "Photo 3",
-          width: 200,
-          height: 300,
-        },
-        {
-          src: require("@/assets/images/image7-preview.jpg"),
-          fullSrc: require("@/assets/images/image7.jpg"),
-          alt: "Photo 3",
-          width: 400,
-          height: 300,
-        },
-        {
-          src: require("@/assets/images/image8-preview.jpg"),
-          fullSrc: require("@/assets/images/image8.jpg"),
-          alt: "Photo 3",
-          width: 400,
-          height: 300,
-        },
-     
-        {
-          src: require("@/assets/images/image9-preview.jpg"),
-          fullSrc: require("@/assets/images/image9.jpg"),
-          alt: "Photo 3",
-          width: 400,
-          height: 300,
-        },
-        {
-          src: require("@/assets/images/image10-preview.jpg"),
-          fullSrc: require("@/assets/images/image10.jpg"),
-          alt: "Norway, ",
-          width: 400,
-          height: 300,
-        }
-      ],
+      photos: []
     };
   },
   methods: {
@@ -125,11 +50,19 @@ export default {
   },
   mounted() {
     window.addEventListener("keydown", this.handleEscape);
+    const jsonUrl = process.env.VUE_APP_JSON_URL;
+   
+    fetch(jsonUrl)
+      .then(response => response.json())
+      .then(data => {
+        this.photos = data;
+      });
   },
   beforeUnmount() {
     window.removeEventListener("keydown", this.handleEscape);
   },
 };
+
 </script>
 
 <style scoped>
